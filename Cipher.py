7962df8078e1744170c2a50bc957452cd19e7822
Amon Daniel <112017439+Genesys-jr@users.noreply.github.com>
2023-06-05 22:56:04 +0100

    Add files via upload

--- Cipher.py
+++ Cipher.py
@@ -1,26 +1,26 @@
-letters = 'abcdefghijklmnopqrstuvwxyz'
-num_letters = len(letters)
+letters = 'abcdefghijklmnopqrstuvwxyz'  # Define the letters of the alphabet
+num_letters = len(letters)  # Calculate the number of letters
 
 def encrypt_decrypt(text, mode, key):
-    result = ''
+    result = ''  # Initialize an empty string to store the result
     if mode == 'd':
-        key = -key
+        key = -key  # If decryption mode is selected, negate the key to reverse the encryption
       
     for letter in text:
-        letter = letter.lower()
+        letter = letter.lower()  # Convert the letter to lowercase
         if not letter == ' ':
-            index = letters.find(letter)
+            index = letters.find(letter)  # Find the index of the letter in the alphabet
             if index == -1:
-                result += letter
+                result += letter  # If the letter is not found in the alphabet, add it as is
             else:
-                new_index = index + key
+                new_index = index + key  # Apply the key to shift the index
                 if new_index >= num_letters:
-                    new_index -= num_letters
+                    new_index -= num_letters  # Wrap around to the beginning of the alphabet if needed
                 elif new_index < 0: 
-                    new_index += num_letters
-                result += letters[new_index]
+                    new_index += num_letters  # Wrap around to the end of the alphabet if needed
+                result += letters[new_index]  # Add the new letter to the result
         else:
-            result += ' '
+            result += ' '  # If the character is a space, add it to the result without encryption/decryption
     return result
 
 
@@ -29,21 +29,21 @@ print('*** CAESAR CIPHER PROGRAM ***')
 print()
 
 print('Do you want to encrypt or decrypt?')
-user_input = input('e/d: ').lower()
+user_input = input('e/d: ').lower()  # Prompt the user for encryption or decryption mode
 print()
 
 if user_input == 'e':
     print('ENCRYPTION MODE SELECTED')
     print()
-    key = int(input('Enter The Key (1 through 26): '))
-    text = input('Enter the text to encrypt: ')
-    ciphertext = encrypt_decrypt(text, user_input, key) 
-    print(f'CIPHERTEXT: {ciphertext}')
+    key = int(input('Enter The Key (1 through 26): '))  # Prompt the user for the encryption key
+    text = input('Enter the text to encrypt: ')  # Prompt the user for the text to encrypt
+    ciphertext = encrypt_decrypt(text, user_input, key)  # Encrypt the text using the provided key
+    print(f'CIPHERTEXT: {ciphertext}')  # Print the encrypted ciphertext
    
 elif user_input == 'd':
     print('DECRYPTION MODE SELECTED')
     print()
-    key = int(input('Enter The Key (1 through 26): '))
-    text = input('Enter the text to decrypt: ')
-    plaintext = encrypt_decrypt(text, user_input, key)
-    print(f'PLAINTEXT: {plaintext}')
+    key = int(input('Enter The Key (1 through 26): '))  # Prompt the user for the decryption key
+    text = input('Enter the text to decrypt: ')  # Prompt the user for the text to decrypt
+    plaintext = encrypt_decrypt(text, user_input, key)  # Decrypt the text using the provided key
+    print(f'PLAINTEXT: {plaintext}')  # Print the decrypted plaintext
